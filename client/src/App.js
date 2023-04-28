b20bf2eedff08a5c322363e0f16cc5adbfd550cb
MuhammedTahaCer <cermuhammed720@gmail.com>
2023-04-28 14:55:04 +0300

    Project initialized,  components are created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//import all modules from react router:
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-function App() {
+/* import all components (uı pages) from conponents folder */
+import Username from  './components/Username';
+import Password from  './components/Password';
+import Register from  './components/Register';
+import Recovery from  './components/Recovery';
+import Reset from  './components/Reset';
+import Profile from  './components/Profile';
+import PageNotFound from  './components/PageNotFound';
+
+
+/* root routers */
+const router=createBrowserRouter([
+    {
+        path: '/',
+        element: <Username></Username>
+    },
+    {
+        path: '/register',
+        element: <Register></Register>
+    },
+    {
+        path: '/password',
+        element: <Password></Password>
+    },
+    {
+        path: '/recovery',
+        element: <Recovery></Recovery>
+    },
+    {
+        path: '/reset',
+        element: <Reset></Reset>
+    },
+    {
+        path: '/profile',
+        element: <Profile></Profile>
+    },
+    {
+        // path: '*' -> speciftyin abstract
+            /** if there is an invalid route... */
+        path: '*',
+        element: <PageNotFound></PageNotFound>
+    }
+])
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <h1> HEllo My App</h1>
-    
-    </div>
-  );
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+        {/* <RouterProvider router={}></RouterProvider> -> Router çağırma örneği */}
+    </main>
+  )
 }
-
-export default App;
---
+++ client/src/components/Username.js
@@ -0,0 +1,10 @@
+//rfc: react functional component
+import React from 'react'
+
+export default function Username() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
